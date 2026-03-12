1930933db105c6253974fde053d30befef649d80
alba <alba_alba111@mail.ru>
2026-03-12 19:44:15 +0300

    base of web api service, gitignore fillings

---
+++ WebApplication1/Contracts/UserProfile.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1.Contracts;
+
+public class UserProfile
+{
+    string FirstName { get; set; }
+    string LastName { get; set; }
+    string Email { get; set; }
+    string Password { get; set; }
+}
